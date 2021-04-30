1f149cb04227aa2639fedbcdb0d2ca44d8f90a3b
caoxinyu <caoxinyu@sinovatech.com>
2021-04-30 13:46:40 +0800

    第一次提交

---
+++ src/test/java/com/example/mypic/MyPicApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.mypic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyPicApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
